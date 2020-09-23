45346cb6e239c04f0e4a5e73c9267d049f58b440
alireza7887 <71773910+alireza7887@users.noreply.github.com>
2020-09-23 20:33:56 +0430

    basic calculator
    This calculator is console-based.

---
+++ basic calculator.java
@@ -0,0 +1,16 @@
+package ITC315;
+
+import java.util.Scanner;
+
+public class Calculator {
+  
+  public void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("1. addition\n2. subtraction\n3. multiplication\n4. division\n\nEnter number of a function you want to do: ");
+    int choice = input.nextInt();
+  
+  }
+
+}
+
