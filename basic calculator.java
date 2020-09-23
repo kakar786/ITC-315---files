91c577563dccbef1a8c4098517f380a6f851460f
alireza7887 <71773910+alireza7887@users.noreply.github.com>
2020-09-23 20:43:00 +0430

    basic calculator v0.5
    The "switch" was added for selection of calculator function.

--- basic calculator.java
+++ basic calculator.java
@@ -5,12 +5,39 @@ import java.util.Scanner;
 public class Calculator {
   
   public void main(String[] args) {
-    
     Scanner input = new Scanner(System.in);
     System.out.println("1. addition\n2. subtraction\n3. multiplication\n4. division\n\nEnter number of a function you want to do: ");
     int choice = input.nextInt();
-  
+    
+    decision(choice);
+    
+    
   }
-
+  
+  
+  public void decision(int a) {
+    switch(a) {
+      case 1:
+        addition();
+        break;
+      case 2:
+        subtraction();
+        break;
+      case 3:
+        multiplication();
+        break;
+      case 4:
+        division();
+        break;
+      default:
+        System.out.println("Invalid input");
+    }
+  }
+  
+  
+  
+  
+  
+  
 }
 
